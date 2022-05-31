d0603ee76d0e4bed114003605c982818c670f11b
macarron001 <rpinoon16@gmail.com>
2022-05-31 13:42:29 +0800

    added spec model test for stock

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'validating stock associations' do
+    it "has many users" do
+      should respond_to(:users)
+    end
+  end
+
+  context 'validating client' do
+    it "get client through iex:api" do
+      client = Stock.get_client
+
+      expect(client.publishable_token).to eq(ENV['PUBLISHABLE_TOKEN'])
+      expect(client.secret_token).to eq(ENV['SECRET_TOKEN'])
+    end
+  end
 end
