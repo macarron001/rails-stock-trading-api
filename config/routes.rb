8fbf8905f605b8532c3b54b146758c51b8dfa314
macarron001 <rpinoon16@gmail.com>
2022-05-31 14:03:23 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-get '/current_user', to: 'current_user#index'
+  resources :posts
+  get '/current_user', to: 'current_user#index'
+  get '/stocks', to: 'stocks#stock_list'
+  get '/pending', to: 'current_user#pending'
+  get '/traders', to: 'current_user#traders'
+  get '/trader', to: 'current_user#trader'
+  post '/create', to: 'current_user#create'
+  post '/edit', to: 'current_user#edit_details'
+  post '/approve', to: 'current_user#approve_user'
+
+  post '/buy', to: 'transactions#buy'
+  post '/sell', to: 'transactions#sell'
+  post '/transaction', to: 'transactions#create_transaction'
+  
+  get '/transactions', to: 'transactions#index'
+  get '/portfolio', to: 'stocks#current_stocks'
+  get '/quote', to: 'stocks#get_quote'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -10,3 +27,5 @@ get '/current_user', to: 'current_user#index'
     registrations: 'users/registrations'
   }
 end
+
+
