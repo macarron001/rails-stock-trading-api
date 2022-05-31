7b68f3725a2f4ff9049b5ac98c21c7cbce23efd2
macarron001 <rpinoon16@gmail.com>
2022-05-31 15:03:20 +0800

    added confirmable index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_052104) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_070159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_052104) do
     t.string "role", default: "trader"
     t.boolean "approved", default: false
     t.bigint "balance", default: 1000
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
