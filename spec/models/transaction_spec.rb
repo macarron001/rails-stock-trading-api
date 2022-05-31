6c8e888a0c03b5f3a347711e7915fdb2d92e86e9
macarron001 <rpinoon16@gmail.com>
2022-05-31 13:33:38 +0800

    added spec model test for transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validate transaction relationship to user' do
+    it "belongs to a user" do
+      transaction = Transaction.new
+      user = User.new
+      user.transactions << transaction
+      expect(transaction.user).to be user
+    end
+  end
+
+  context 'validate creation of "buy" transaction record' do
+    it "should create a 'buy' transaction record" do
+      Stock.create!(symbol: 'SOFI', company_name: 'SOFI', price: 2)
+      user = User.create!(email: "hello@test.com", password: "password", id: 15)
+      user.buy_stock('SOFI', 1, user.id)
+
+      trader = User.find(15)
+      expect(trader.transactions.count).to eq 1
+    end
+  end
+
+  context 'validate creation of "sell" transaction record' do
+    it "should create a 'sell' transaction record" do
+      Stock.create!(symbol: 'SOFI', company_name: 'SOFI', price: 2)
+      user = User.create!(email: "hello@test.com", password: "password", id: 15)
+      user.sell_stock('SOFI', 1, user.id)
+
+      trader = User.find(15)
+      expect(trader.transactions.count).to eq 1
+    end
+  end
+
+  context 'validate update of user balance' do
+    it "should subtract total from the balance" do
+      Stock.create!(symbol: 'SOFI', company_name: 'SOFI', price: 2)
+      user = User.create!(email: "hello@test.com", password: "password", id: 15)
+      user.buy_stock('SOFI', 1, user.id)
+
+      trader = User.find(15)
+      expect(trader.balance).to be < 10000
+    end
+
+    it "should add total to the balance" do
+      Stock.create!(symbol: 'SOFI', company_name: 'SOFI', price: 2)
+      user = User.create!(email: "hello@test.com", password: "password", id: 15)
+      user.sell_stock('SOFI', 1, user.id)
+
+      trader = User.find(15)
+      expect(trader.balance).to be > 100
+    end
+  end
+
 end
