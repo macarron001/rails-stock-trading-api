1d0073bec074316989f268f6c964168dce424481
macarron001 <rpinoon16@gmail.com>
2022-05-31 14:18:16 +0800

    fixed logout code logic bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,16 +10,9 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if current_user
-      render json: {
-        status: 200,
-        message: "logged out successfully"
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
-    end
+    render json: {
+      status: 200,
+      message: "logged out successfully"
+    }, status: :ok
   end
 end
