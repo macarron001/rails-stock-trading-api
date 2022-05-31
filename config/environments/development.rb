b54591b475847f348b902335d0d9e4e418692631
macarron001 <rpinoon16@gmail.com>
2022-05-31 14:17:46 +0800

    added mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,11 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.delivery_method = :smtp
+  #SMTP settings for mailcatcher gem
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
+  config.action_mailer.raise_delivery_errors = false
 end
